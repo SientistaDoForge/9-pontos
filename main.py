d4b5d2c1b856fdc3aea52ff21c5729394c60d63f
SientistaDoForge <sientistadoforge@gmail.com>
2026-05-05 18:05:12 +0100

    9 pontos

---
+++ main.py
@@ -0,0 +1,17 @@
+from manim import *
+
+
+config.pixel_height = 1080
+config.pixel_width = 1920
+config.frame_height = 7.0  # physical units for scene height
+config.frame_width = 12.0  # physical units for scene width
+config.frame_rate = 60
+
+
+class Main(Scene):
+	def construct(self):
+		text1 = Text("Ola a todos").shift(UP)
+
+		# Step 1
+		self.play(Write(text1))
+		input("Press ENTER to continue...")  # waits for user input
