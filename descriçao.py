296570d5a4f887d8e660e18d53d2139c670d41c9
SientistaDoForge <sientistadoforge@gmail.com>
2026-05-05 18:48:19 +0100

    9 pontos

--- descriçao.py
+++ descriçao.py
@@ -6,15 +6,4 @@ config.pixel_width = 1920
 config.frame_height = 7.0  # physical units for scene height
 config.frame_width = 12.0  # physical units for scene width
 config.frame_rate = 60
-
-
-class Main(Scene):
-	def construct(self):
-		text1 = Text("Ola a todos").shift(UP)
-
-		# Step 1
-		self.play(Write(text1))
-		input("Press ENTER to continue...")  # waits for user input
-
-
-#gonçaloide
+config.media_dir = "./output"
